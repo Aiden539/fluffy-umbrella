66158884d1eb6bd2e21be381c57ace8c103eef0a
Aiden539 <85898652+Aiden539@users.noreply.github.com>
2021-06-14 10:56:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//alert('Hello world');
+document.querySelector("h1").addEventListener("click",function(){
+document.querySelector("h1").style.backgroundColor ="black"
+
+})
+
+document.querySelector("ol").addEventListener("click",function(){
+document.querySelector("ol").style.backgroundColor ="brown"
+document.querySelector("ol").style.color ="red"
+document.querySelector("ol").style.textShadow='90px 90px black'
+
+})
+
+
+document.querySelector("p").addEventListener("click",function(){
+document.querySelector("p").style.fontSize ='60px'
+
+})
+
